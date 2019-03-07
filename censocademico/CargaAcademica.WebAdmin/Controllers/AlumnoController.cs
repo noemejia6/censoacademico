1f17031506f7bcbf36ac149e6c89415be06f82d0
Noe Mejia <Noe Mejia@DESKTOP-9B3374E>
2019-03-07 17:25:04 -0600

    modificando proyecto

---
+++ censocademico/CargaAcademica.WebAdmin/Controllers/AlumnoController.cs
@@ -0,0 +1,112 @@
+﻿using CargaAcademica.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CargaAcademica.WebAdmin.Controllers//paso 4 controlador para las acciones html
+{
+    public class AlumnoController : Controller
+    {
+        AlumnoBL _AlumnoBL;
+
+        public AlumnoController()
+        {
+            _AlumnoBL = new AlumnoBL();
+        }
+
+
+
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            var ListadeAlumnos = _AlumnoBL.ObtenerAlumnos();
+
+            return View(ListadeAlumnos);
+        }
+
+        public ActionResult Crear()
+        {
+            var nuevoAlumno = new Alumno();
+
+            return View(nuevoAlumno);
+        }
+
+        [HttpPost]//validacion
+        public ActionResult Crear(Alumno alumno, HttpPostedFileBase imagen)
+        {
+            if (ModelState.IsValid)//validacion con estructura if que evalua si el model state es valio.
+                                  //entonces permite guardar. 
+            {
+                _AlumnoBL.GuardarAlumno(alumno);
+
+                return RedirectToAction("Index");//pendiente index
+            }
+
+            if (imagen != null)
+                {
+                alumno.UrlImagen = GuardarImagen(imagen);
+                }
+
+            return View(alumno);
+        }
+
+
+        public ActionResult Editar(int Id)
+        {
+            var alumno = _AlumnoBL.ObtenerAlumnos(Id);
+
+            return View(alumno);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                /*if(alumno.Id != alumno.Id.Trim())
+                {
+
+                }*/
+                _AlumnoBL.GuardarAlumno(alumno);
+
+                return RedirectToAction("Index");//pendiente index tambien
+            }
+
+            return View(alumno);
+        }
+
+
+        public ActionResult Detalle(int Id)
+        {
+            var alumno = _AlumnoBL.ObtenerAlumnos(Id);
+
+            return View(alumno);
+        }
+
+
+        public ActionResult Eliminar(int Id)
+        {
+            var alumno = _AlumnoBL.ObtenerAlumnos(Id);
+
+            return View(alumno);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(Alumno alumno)
+        {
+            _AlumnoBL.EliminarAlumno(alumno.Id);
+
+            return RedirectToAction("Index");//pendiente index
+        }
+
+        private string GuardarImagen(HttpPostedFileBase imagen)
+        {
+            string path = Server.MapPath("~/Imagenes/" + imagen.FileName);//para sacar ese simbolo uso alt + 126
+
+            return "/imagenes/" + imagen.FileName;
+
+        }
+    }
+}
